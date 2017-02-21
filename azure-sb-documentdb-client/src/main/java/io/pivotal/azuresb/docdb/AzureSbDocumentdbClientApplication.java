5b786bd7eff7fb675ce07c72102698341dc35d10
boparp-pivotal <pbopardikar@pivotal.io>
2017-02-21 14:34:42 -0800

    refactoring done, new REST Controller class added

--- azure-sb-documentdb-client/src/main/java/io/pivotal/azuresb/docdb/AzureSbDocumentdbClientApplication.java
+++ azure-sb-documentdb-client/src/main/java/io/pivotal/azuresb/docdb/AzureSbDocumentdbClientApplication.java
@@ -1,88 +1,13 @@
 package io.pivotal.azuresb.docdb;
 
-import io.pivotal.azuresb.docdb.dao.TodoDao;
-import io.pivotal.azuresb.docdb.model.TodoItem;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
 @SpringBootApplication
 public class AzureSbDocumentdbClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSbDocumentdbClientApplication.class, args);
-	}
-
-	@Autowired
-	@Qualifier("DocDbDaoInstance")
-	private TodoDao todoDao;
-
-	@RequestMapping("/greeting")
-	List<String> greet(String name) {
-		List<String> greetings = new ArrayList<String>();
-		greetings.add("Hello " + name + "!");
-		greetings.add("Hola " + name + "!");
-		greetings.add("Namaste " + name + "!");
-		return greetings;
-	}
-
-	@RequestMapping("/read")
-	List<TodoItem> readTodoItems() {
-		List<TodoItem> todoItems = null;
-		try {
-			todoItems = todoDao.readTodoItems();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return todoItems;
-	}
-
-	@RequestMapping("/write")
-	TodoItem createTodoItem(String name, String category) {
-		TodoItem todoItemCreated = null;
-		TodoItem todoItem = new TodoItem();
-		todoItem.setId(String.valueOf(System.currentTimeMillis()));
-		todoItem.setComplete(false);
-		todoItem.setCategory(category);
-		todoItem.setName(name);
-		try {
-			todoItemCreated = todoDao.createTodoItem(todoItem);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return todoItemCreated;
-	}
-
-	@RequestMapping("/update")
-	TodoItem updateTodoItem(String id, boolean isComplete) {
-		TodoItem todoItemUpdated = null;
-		try {
-			todoItemUpdated = todoDao.updateTodoItem(id, isComplete);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return todoItemUpdated;
-	}
-
-	@RequestMapping("/delete")
-	boolean deleteTodoItem(String id) {
-		boolean deleted = false;
-		try {
-			deleted = todoDao.deleteTodoItem(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return deleted;
 	}
 
 }
---
+++ azure-sb-documentdb-client/src/main/java/io/pivotal/azuresb/docdb/DocumentDbRestController.java
@@ -0,0 +1,88 @@
+package io.pivotal.azuresb.docdb;
+
+import io.pivotal.azuresb.docdb.dao.TodoDao;
+import io.pivotal.azuresb.docdb.model.TodoItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author pbopardikar This is the REST Controller class that implements all the
+ *         REST CRUD methods for the DocumentDB
+ */
+@RestController
+@EnableAutoConfiguration
+public class DocumentDbRestController {
+
+	@Autowired
+	@Qualifier("DocDbDaoInstance")
+	private TodoDao todoDao;
+
+	@RequestMapping("/greeting")
+	private List<String> greet(String name) {
+		System.out.println("Dummy Test Method");
+		List<String> greetings = new ArrayList<String>();
+		greetings.add("Hello " + name + "!");
+		greetings.add("Hola " + name + "!");
+		greetings.add("Namaste " + name + "!");
+		greetings.add("Bon Jour " + name + "!");
+		return greetings;
+	}
+
+	@RequestMapping("/read")
+	private List<TodoItem> readTodoItems() {
+		List<TodoItem> todoItems = null;
+		try {
+			todoItems = todoDao.readTodoItems();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return todoItems;
+	}
+
+	@RequestMapping("/write")
+	private TodoItem createTodoItem(String name, String category) {
+		TodoItem todoItemCreated = null;
+		TodoItem todoItem = new TodoItem();
+		todoItem.setId(String.valueOf(System.currentTimeMillis()));
+		todoItem.setComplete(false);
+		todoItem.setCategory(category);
+		todoItem.setName(name);
+		try {
+			todoItemCreated = todoDao.createTodoItem(todoItem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return todoItemCreated;
+	}
+
+	@RequestMapping("/update")
+	private TodoItem updateTodoItem(String id, boolean isComplete) {
+		TodoItem todoItemUpdated = null;
+		try {
+			todoItemUpdated = todoDao.updateTodoItem(id, isComplete);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return todoItemUpdated;
+	}
+
+	@RequestMapping("/delete")
+	private boolean deleteTodoItem(String id) {
+		boolean deleted = false;
+		try {
+			deleted = todoDao.deleteTodoItem(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return deleted;
+	}
+
+}
